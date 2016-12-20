a5655a80973e49830af25a7c6c40376bfcb6178d
BuildTools <unconfigured@null.spigotmc.org>
2016-12-20 02:23:03 -0500

    Removed Unneccessary comments from script to oop

--- src/com/nayoshi12/christmasCracker/ChristmasCracker.java
+++ src/com/nayoshi12/christmasCracker/ChristmasCracker.java
@@ -16,8 +16,7 @@ import java.util.*;
  * Created by Matthew on 12/19/2016.
  */
 public class ChristmasCracker extends JavaPlugin {
-    //    private TreeMap<Integer,ItemStack> rewards = new TreeMap<>();
-//    private ItemStack christmas;
+
     private Random random = new Random();
     private RewardManager rewardManager;
     private String prefix;
@@ -30,10 +29,6 @@ public class ChristmasCracker extends JavaPlugin {
         cCracker = new CCracker(this);
         this.getCommand("christmascracker").setExecutor(new MainExecutor(this));
         this.getServer().getPluginManager().registerEvents(new PlayerClickHandler(this),this);
-//        initItems();
-//        initChristmasCracker();
-//        initListener();
-//        initCommands();
     }
     public RewardManager getRewardManager(){
         return rewardManager;
@@ -47,57 +42,5 @@ public class ChristmasCracker extends JavaPlugin {
     public CCracker getcCracker(){
         return cCracker;
     }
-//    public void initItems(){
-//        rewards.clear();
-//        int total = 0;
-//        for(String key:this.getConfig().getConfigurationSection("rewards").getKeys(false)){
-//            Material mat = Material.valueOf(getConfig().getString("rewards."+key+".type"));
-//            int amnt = this.getConfig().getInt("rewards."+ key +".amount");
-//            int dura =(int) this.getConfig().get("rewards."+ key +".durability");
-//            ItemStack item = new ItemStack(mat,amnt,(short)dura);
-//            ItemMeta im = item.getItemMeta();
-//            im.setDisplayName(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("rewards."+ key +".name")));
-//            List<String> lore = this.getConfig().getStringList("rewards." + key + ".lore");
-//            List<String> lore1 = new ArrayList<>();
-//            for(String l:lore){
-//                lore1.add(ChatColor.translateAlternateColorCodes('&',l));
-//            }
-//            im.setLore(lore1);
-//            im.spigot().setUnbreakable(this.getConfig().getBoolean("rewards."+ key + ".unbreakable"));
-//            int chance = this.getConfig().getInt("rewards.$key.chance");
-//            total += chance;
-//            item.setItemMeta(im);
-//            rewards.put(total, item);
-//        }
-//    }
-//    public TreeMap<Integer,ItemStack> getRewards(){
-//        return rewards;
-//    }
 
-//    public ItemStack getChristmas(){
-//        return christmas;
-//    }
-//    private void initCommands() {
-//        getCommand("christmascracker").setExecutor(new MainExecutor(this));
-//    }
-//
-//    private void initListener() {
-//        this.getServer().getPluginManager().registerEvents(new PlayerClickHandler(this),this);
-//    }
-//
-//    private void initChristmasCracker() {
-//        Material mat = Material.valueOf(this.getConfig().getString("christmascracker.type"));
-//        int dura = (int)this.getConfig().get("christmascracker.durability");
-//        christmas = new ItemStack(mat,1,(short)dura);
-//        ItemMeta im = christmas.getItemMeta();
-//        im.setDisplayName(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("christmascracker.name")));
-//        List<String> lore = this.getConfig().getStringList("christmascracker.lore");
-//        List<String> lore1 = new ArrayList<>();
-//        for(String l:lore){
-//            lore1.add(ChatColor.translateAlternateColorCodes('&',l));
-//        }
-//        im.setLore(lore1);
-//        im.spigot().setUnbreakable(this.getConfig().getBoolean("christmascracker.unbreakable"));
-//        christmas.setItemMeta(im);
-//    }
 }
